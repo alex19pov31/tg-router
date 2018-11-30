3766168c4562dad32bf4489c9fe19e9a13ac9d6e
Alexandr Nesterov <nesterov@greensight.ru>
2018-11-30 15:20:59 +0300

    add query callback route

--- tg-router.go
+++ tg-router.go
@@ -12,12 +12,24 @@ type routeCallback func(update tgbotapi.Update)
 
 type route struct {
 	comand       string
+	queryData    []byte
 	containText  string
 	pregTemplate string
 	callback     routeCallback
 }
 
-func (r *route) check(context string, callback chan routeCallback) {
+func (r *route) check(update tgbotapi.Update, callback chan routeCallback) {
+	if update.CallbackQuery != nil && len(r.queryData) > 0 &&
+		string(r.queryData) == string(update.CallbackQuery.Data) {
+		callback <- r.callback
+		return
+	}
+
+	if update.Message == nil {
+		return
+	}
+
+	context := update.Message.Text
 	if r.comand != "" && r.comand == context {
 		callback <- r.callback
 		return
@@ -42,6 +54,11 @@ type RouteGroup struct {
 	callback chan routeCallback
 }
 
+// AddQueryRoute - роутер для Callback запросов
+func (rg *RouteGroup) AddQueryRoute(data []byte, callback routeCallback) {
+	rg.routes = append(rg.routes, route{queryData: data, callback: callback})
+}
+
 // AddCommandRoute - роутер для простой команды
 func (rg *RouteGroup) AddCommandRoute(comand string, callback routeCallback) {
 	rg.routes = append(rg.routes, route{comand: comand, callback: callback})
@@ -59,11 +76,12 @@ func (rg *RouteGroup) AddPregRoute(pregTemplate string, callback routeCallback) 
 
 // Run - запуск роутера
 func (rg *RouteGroup) Run(update tgbotapi.Update) {
-	go func(context string, callback chan routeCallback) {
-		for _, route := range rg.routes {
-			go route.check(context, callback)
-		}
-	}(update.Message.Text, rg.callback)
+	rg.callback = make(chan routeCallback)
+	for _, rt := range rg.routes {
+		go func(update tgbotapi.Update, rt route, callback chan routeCallback) {
+			rt.check(update, callback)
+		}(update, rt, rg.callback)
+	}
 
 	go func(rg *RouteGroup) {
 		select {
